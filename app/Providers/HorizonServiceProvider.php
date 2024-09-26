b59d720d38b635e2e38c57a3d734f7fba6ff9e5f
Mauro Casas <maurocasas.arg@gmail.com>
2024-09-26 01:30:49 -0300

    horizon viewer

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\UserRole;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
@@ -28,9 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
-                'maurocasas.arg@gmail.com',
-            ]);
+            return $user->role->value === UserRole::ADMIN->value;
         });
     }
 }
