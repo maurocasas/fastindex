0e1e375b1ea223f1d2aa2d7ed0498ce94501b1e7
Mauro Casas <maurocasas.arg@gmail.com>
2024-09-27 19:33:37 -0300

    Page upserting for massive sitemaps

--- app/Jobs/Pages/ListPagesBySitemap.php
+++ app/Jobs/Pages/ListPagesBySitemap.php
@@ -44,34 +44,33 @@ class ListPagesBySitemap implements ShouldQueue
             return;
         }
 
-        $existingPages = $this->sitemap->site->pages()->pluck('url')->toArray();
-
         $pages = [];
 
         foreach ($xml->url as $item) {
-            $url = (string) $item->loc;
-            $path = Str::after($url, $this->sitemap->site->hostname);
-
-            if (in_array($url, $existingPages)) {
-                continue;
-            }
-
-            $pages[] = [
-                'site_id' => $this->sitemap->site_id,
-                'url' => (string)$item->loc,
-                'path' => blank($path) ? '/' : $path,
-            ];
+            $pages[] = (string) $item->loc;
         }
 
-        DB::transaction(function () use ($pages) {
-            DB::table('pages')->insert($pages);
-        });
+        LazyCollection::make($pages)
+            ->chunk(1000)
+            ->each(fn($chunk) => $this->upsertChunk($chunk));
 
         $this->sitemap->toggleBusy(false);
     }
 
     private function upsertChunk($chunk)
     {
+        DB::transaction(function () use ($chunk) {
+            $values = $chunk->map(function ($url) {
+                $path = Str::after($url, $this->sitemap->site->hostname);
 
+                return [
+                    'site_id' => $this->sitemap->site_id,
+                    'url' => $url,
+                    'path' => blank($path) ? '/' : $path,
+                ];
+            })->toArray();
+
+            $this->sitemap->site->pages()->upsert($values, ['url']);
+        });
     }
 }
